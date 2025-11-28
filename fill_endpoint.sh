06c45039e7892cdc1f2c3dc60aec9cb021bb9e37
RajpootDeepak <insta8219743056>
2025-11-28 20:15:44 +0530

    bug fix in api endpoint script

--- fill_endpoint.sh
+++ fill_endpoint.sh
@@ -3,5 +3,5 @@ set -e
 if [ -z "$1" ]; then echo "Usage: ./fill_endpoint.sh <api_endpoint>"; exit 1; fi
 EP="$1"
 if [ ! -f "index.html" ]; then echo "index.html not found"; exit 1; fi
-perl -0777 -pe "s/__CONTACT_API_ENDPOINT__/\"${EP}/g" -i index.html
+perl -0777 -pe "s/__CONTACT_API_ENDPOINT__/\"${EP}"/g" -i index.html
 echo "Replaced placeholder with $EP"
